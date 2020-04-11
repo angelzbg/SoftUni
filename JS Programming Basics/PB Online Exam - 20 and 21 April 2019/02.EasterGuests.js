3a8d669156ec169ef3b9f8ae67d0e2fdb931f69b
angelzbg <badblo0dbg@gmail.com>
2020-04-11 19:45:20 +0300

    cleanup

--- JS Programming Basics/PB Online Exam - 20 and 21 April 2019/02.EasterGuests.js
+++
@@ -1,8 +0,0 @@
-(input) => {
-    let guests = Number(input.shift()), budget = Number(input.shift()), kozunaci = Math.ceil(guests/3), qica = guests*2;
-
-    let price = kozunaci*4 + qica*0.45, diff = budget - price;
-
-    if(diff >= 0) console.log(`Lyubo bought ${kozunaci} Easter bread and ${qica} eggs.\nHe has ${diff.toFixed(2)} lv. left.`);
-    else console.log(`Lyubo doesn't have enough money.\nHe needs ${Math.abs(diff).toFixed(2)} lv. more.`);
-}
