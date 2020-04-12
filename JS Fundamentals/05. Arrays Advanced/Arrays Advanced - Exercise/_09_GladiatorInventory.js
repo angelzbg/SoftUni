df339b11af3434534c7e4b751d5e4d39944ae9c3
angelzbg <badblo0dbg@gmail.com>
2020-04-12 18:08:36 +0300

    100/100

--- JS Fundamentals/05. Arrays Advanced/Arrays Advanced - Exercise/_09_GladiatorInventory.js
+++ JS Fundamentals/05. Arrays Advanced/Arrays Advanced - Exercise/_09_GladiatorInventory.js
@@ -1,10 +1,11 @@
 gladiatorInventory = (input) => {
-    // Porednata piqna zadacha ot softuni
     let inventory = input.shift().split(' ');
     input.forEach(line => {
         let [ cmd, args ] = line.split(' ');
-        if(cmd === 'Buy' && inventory.indexOf(args) === -1) {
-            inventory.push(args);
+        if(cmd === 'Buy') {
+            if(inventory.indexOf(args) === -1) {
+                inventory.push(args);
+            }
         } else if(cmd === 'Trash') {
             let index = inventory.indexOf(args);
             if(index > -1) inventory.splice(index, 1);
