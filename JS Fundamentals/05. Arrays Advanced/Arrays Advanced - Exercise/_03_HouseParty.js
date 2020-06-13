8afd0f443013062e5cf78752fc88afe9e0eeae5a
angelzbg <badblo0dbg@gmail.com>
2020-06-13 14:03:45 +0300

    Update _03_HouseParty.js

--- JS Fundamentals/05. Arrays Advanced/Arrays Advanced - Exercise/_03_HouseParty.js
+++ JS Fundamentals/05. Arrays Advanced/Arrays Advanced - Exercise/_03_HouseParty.js
@@ -3,16 +3,12 @@ houseParty = (input) => {
     while(input.length > 0) {
         let cmd = input.shift().split(' ');
         let index = guests.indexOf(cmd[0]);
-        
-        if(cmd[2] === 'going!') {
-            if(index !== -1) {
-                console.log(`${cmd[0]} is already in the list!`);
-            } else {
+
+        if(index !== -1) {
+            console.log(`${cmd[0]} is ${cmd[2] === 'going!' ? 'already' : 'not'} in the list!`);
+        } else {
+            if(cmd[2] === 'going!') {
                 guests.push(cmd[0]);
-            }
-        } else { // not
-            if(index === -1) {
-                console.log(`${cmd[0]} is not in the list!`);
             } else {
                 guests.splice(index, 1);
             }
@@ -21,3 +17,19 @@ houseParty = (input) => {
 
     console.log(guests.join('\n'));
 };
+
+/*
+if(cmd[2] === 'going!') {
+    if(index !== -1) {
+        console.log(`${cmd[0]} is already in the list!`);
+    } else {
+        guests.push(cmd[0]);
+    }
+} else { // not
+    if(index === -1) {
+        console.log(`${cmd[0]} is not in the list!`);
+    } else {
+        guests.splice(index, 1);
+    }
+}
+*/
