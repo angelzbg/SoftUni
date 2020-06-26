be47243ed93c074b7adedfa5f9795235c3da7817
Angel Zlatanov <angel.zlatanovv@gmail.com>
2020-06-27 00:31:41 +0300

    Update _05_Inventory.js

--- JS Fundamentals/06. Objects and Classes/Objects and Classes - Exercise/_05_Inventory.js
+++ JS Fundamentals/06. Objects and Classes/Objects and Classes - Exercise/_05_Inventory.js
@@ -1,11 +1,11 @@
 inventory = (heroes = []) => {
-    heroes = heroes.map(h => {
-        let [ name, level, items ] = h.split(' / ');
-        level = Number(level);
-        items = items.split(', ').sort().join(', ');
-        return { name, level, items };
-    });
-    
-    heroes.sort((h1, h2) => h1.level - h2.level);
-    heroes.forEach(h => console.log(`Hero: ${h.name}\nlevel => ${h.level}\nitems => ${h.items}`));
+    heroes
+        .map(h => {
+            let [ name, level, items ] = h.split(' / ');
+            level = Number(level);
+            items = items.split(', ').sort().join(', ');
+            return { name, level, items };
+        })
+        .sort((h1, h2) => h1.level - h2.level)
+        .forEach(h => console.log(`Hero: ${h.name}\nlevel => ${h.level}\nitems => ${h.items}`));
 };
