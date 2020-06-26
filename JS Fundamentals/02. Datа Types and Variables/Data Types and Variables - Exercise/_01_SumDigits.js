57956074a6a6dce7009631a4e148fcff60478a0f
Angel Zlatanov <angel.zlatanovv@gmail.com>
2020-06-27 00:37:21 +0300

    Update _01_SumDigits.js

--- JS Fundamentals/02. Datа Types and Variables/Data Types and Variables - Exercise/_01_SumDigits.js
+++ JS Fundamentals/02. Datа Types and Variables/Data Types and Variables - Exercise/_01_SumDigits.js
@@ -1,3 +1,7 @@
 sumDigits = (num = 1) => {
-    console.log(num.toString().split('').map(Number).reduce((a, b) => a + b, 0));
+    return num
+        .toString()
+        .split('')
+        .map(Number)
+        .reduce((a, b) => a + b, 0);
 };
