b500cd5a8d47f9df5567b2c6c0c27bc5a594264d
angelzbg <38349591+angelzbg@users.noreply.github.com>
2020-07-03 11:11:32 +0300

    Update _01_WordsTracker.js

--- JS Fundamentals/07. Associative Arrays/Associative Arrays - Exercise/_01_WordsTracker.js
+++ JS Fundamentals/07. Associative Arrays/Associative Arrays - Exercise/_01_WordsTracker.js
@@ -1,18 +1,20 @@
 wordsTracker = (arr = []) => {
-    let words = {};
-    arr.shift().split(' ').forEach(word => {
-        words[word] = 0;
-    });
-
-    arr.forEach(word => {
-        if(words[word]) {
-            words[word]++;
+  return Object.entries(
+    arr.reduce(
+      (words, word) => {
+        if (words[word] !== undefined) {
+          words[word]++;
         }
-    });
-    
-    Object.entries(words)
+
+        return words;
+      },
+      arr
+        .shift()
+        .split(" ")
+        .reduce((initial, word) => Object.assign(initial, { [word]: 0 }), {})
+    )
+  )
     .sort((a, b) => b[1] - a[1])
-    .forEach(([word, count]) => {
-        console.log(`${word} - ${count}`);
-    });
+    .map(([word, count]) => `${word} - ${count}`)
+    .join("\n");
 };
