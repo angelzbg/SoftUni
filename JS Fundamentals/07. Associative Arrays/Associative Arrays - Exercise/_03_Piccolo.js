d2e36fa1aefcd41d07a38bbe2aa0235ecfcf101f
angelzbg <38349591+angelzbg@users.noreply.github.com>
2020-07-03 11:16:06 +0300

    Update _03_Piccolo.js

--- JS Fundamentals/07. Associative Arrays/Associative Arrays - Exercise/_03_Piccolo.js
+++ JS Fundamentals/07. Associative Arrays/Associative Arrays - Exercise/_03_Piccolo.js
@@ -1,9 +1,11 @@
 piccolo = (input = []) => {
-    [...input.reduce((cars, line) => {
-        let [ direction, car ] = line.split(', ');
-        cars[direction === 'IN' ? 'add' : 'delete'](car);
-        return cars;
-    }, new Set())]
+  [
+    ...input.reduce((cars, line) => {
+      let [direction, car] = line.split(", ");
+      cars[direction === "IN" ? "add" : "delete"](car);
+      return cars;
+    }, new Set()),
+  ]
     .sort()
-    .forEach(car => console.log(car));
+    .forEach((car) => console.log(car));
 };
