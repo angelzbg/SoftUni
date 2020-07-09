b35dd2563e9d6526becbaad91cdae1a1bb0c7cbb
angelzbg <38349591+angelzbg@users.noreply.github.com>
2020-07-09 21:11:59 +0300

    Update _02_OddOccurrences.js

--- JS Fundamentals/07. Associative Arrays/Associative Arrays - Exercise/_02_OddOccurrences.js
+++ JS Fundamentals/07. Associative Arrays/Associative Arrays - Exercise/_02_OddOccurrences.js
@@ -1,14 +1,14 @@
-oddOccurrences = (str = "") => {
-  return Object.entries(
-    str
-      .toLowerCase()
-      .split(" ")
-      .reduce((occ, word) => {
-        occ[word] = 1 + (occ[word] || 0);
-        return occ;
-      }, {})
-  )
-    .filter(([key, value]) => value % 2 === 1)
-    .map(([key, value]) => key)
-    .join(" ");
+oddOccurrences = (str = '') => {
+    return Object.entries(
+        str
+            .toLowerCase()
+            .split(' ')
+            .reduce((occ, word) => {
+                occ[word] = 1 + (occ[word] || 0);
+                return occ;
+            }, {}),
+    )
+        .filter(([key, value]) => value % 2 === 1)
+        .map(([key, value]) => key)
+        .join(' ');
 };
