87bfddb7ea0cd1676f2248563356af0216fa9141
angelzbg <38349591+angelzbg@users.noreply.github.com>
2020-07-11 15:23:34 +0300

    Update _05_Neighborhoods.js

--- JS Fundamentals/07. Associative Arrays/Associative Arrays - Lab/_05_Neighborhoods.js
+++ JS Fundamentals/07. Associative Arrays/Associative Arrays - Lab/_05_Neighborhoods.js
@@ -1,20 +1,25 @@
 neighborhoods = (input = []) => {
-    let neighborhoods = {};
-    input.shift().split(', ').forEach(neighborhood => {
-        neighborhoods[neighborhood] = [];
-    });
+    return Object.entries(
+        input
+            .slice(1)
+            .map((line) => line.split(' - '))
+            .reduce(
+                (neighborhoods, [neighborhood, person]) => {
+                    if (neighborhoods[neighborhood]) {
+                        neighborhoods[neighborhood].push(person);
+                    }
 
-    input.forEach(line => {
-        let [ neighborhood, person ] = line.split(' - ');
-        if(neighborhoods[neighborhood]) {
-            neighborhoods[neighborhood].push(person);
-        }
-    });
-    
-    Object.keys(neighborhoods)
-    .sort((a, b) => neighborhoods[b].length - neighborhoods[a].length)
-    .forEach(neighborhood => {
-        console.log(`${neighborhood}: ${neighborhoods[neighborhood].length}`);
-        neighborhoods[neighborhood].forEach(person => console.log(`--${person}`));
-    });
+                    return neighborhoods;
+                },
+                input[0]
+                    .split(', ')
+                    .reduce((neighborhoods, neighborhood) => Object.assign(neighborhoods, { [neighborhood]: [] }), {}),
+            ),
+    )
+        .sort((a, b) => b[1].length - a[1].length)
+        .map(
+            ([neighborhood, people]) =>
+                `${neighborhood}: ${people.length}${people.length ? `\n--${people.join('\n--')}` : ''}`,
+        )
+        .join('\n');
 };
