f661b9c78b2ec031d9ac41cd2022bf0e239cf46b
angelzbg <38349591+angelzbg@users.noreply.github.com>
2020-09-29 11:56:32 +0300

    Update solution.js

--- JS Advanced/04. Lab - DOM/04. Visited Websites/solution.js
+++ JS Advanced/04. Lab - DOM/04. Visited Websites/solution.js
@@ -1,14 +1,10 @@
 solve = () => {
-    const clickElement = (index) => {
-        const paragraphs = document.getElementsByTagName('p');
-        const paragraph = paragraphs[index];
-        const clicks = Number(paragraph.textContent.match(/\d+/g).shift()) + 1;
-        paragraph.innerHTML = `visited ${clicks} times`;
-    };
-    let counter = 0;
-    const elements = document.getElementsByTagName('a');
-    for (element of elements) {
-        const index = counter++;
-        element.addEventListener('click', () => clickElement(index));
-    }
+  const clickElement = (paragraph) =>
+    (paragraph.innerHTML = `visited ${+paragraph.textContent.match(/\d+/g).shift() + 1} times`);
+
+  [...document.getElementsByTagName('a')].forEach((element, index) =>
+    element.addEventListener('click', () =>
+      clickElement(document.querySelector(`div.middled > div:nth-child(${index + 1}) > p`))
+    )
+  );
 };
