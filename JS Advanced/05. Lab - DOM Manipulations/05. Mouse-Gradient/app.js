4bc8d342f11f8b8c8fb6447e8261dc017e07330a
angelzbg <38349591+angelzbg@users.noreply.github.com>
2020-10-06 20:12:45 +0300

    Update app.js

--- JS Advanced/05. Lab - DOM Manipulations/05. Mouse-Gradient/app.js
+++ JS Advanced/05. Lab - DOM Manipulations/05. Mouse-Gradient/app.js
@@ -1,12 +1,9 @@
-attachGradientEvents = () => {
-    const resultDiv = document.getElementById('result');
-    document.getElementById('gradient').addEventListener('mousemove', (e) => {
-        const mouseX = e.offsetX;
-        const width = e.target.clientWidth;
-        const percent = (100 / width) * mouseX;
-        resultDiv.textContent = `${Math.trunc(percent)}%`;
-    });
-    document.getElementById('gradient-box').addEventListener('mouseout', () => {
-        resultDiv.textContent = '';
-    });
+attachGradientEvents = (resultDiv = document.getElementById('result')) => {
+  document
+    .getElementById('gradient')
+    .addEventListener(
+      'mousemove',
+      (e) => (resultDiv.textContent = `${Math.trunc((100 / e.target.clientWidth) * e.offsetX)}%`)
+    );
+  document.getElementById('gradient-box').addEventListener('mouseout', () => (resultDiv.textContent = ''));
 };
