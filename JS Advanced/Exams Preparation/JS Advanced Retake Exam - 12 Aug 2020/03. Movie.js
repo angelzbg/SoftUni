9bae34abeb93fd0e801505c3f16a660c06ead164
angelzbg <38349591+angelzbg@users.noreply.github.com>
2020-10-20 19:59:31 +0300

    Update 03. Movie.js

--- JS Advanced/Exams Preparation/JS Advanced Retake Exam - 12 Aug 2020/03. Movie.js
+++ JS Advanced/Exams Preparation/JS Advanced Retake Exam - 12 Aug 2020/03. Movie.js
@@ -1,46 +1,42 @@
 class Movie {
   constructor(movieName, ticketPrice) {
-    Object.assign(this, { movieName, ticketPrice: +ticketPrice, screenings: [], totalProfit: 0, soldTickets: 0 });
+    ticketPrice = Number(ticketPrice);
+    Object.assign(this, { movieName, ticketPrice, screenings: [], totalProfit: 0, soldTickets: 0 });
   }
 
   newScreening(date, hall, description) {
-    const found = this.screenings.find((el) => el.hall === hall && el.date === date);
-    if (found) {
+    if (this.screenings.find((s) => s.date === date && s.hall === hall)) {
       throw new Error(`Sorry, ${hall} hall is not available on ${date}`);
     }
 
-    this.screenings.push({ hall, date, description });
+    this.screenings.push({ date, hall, description });
     return `New screening of ${this.movieName} is added.`;
   }
 
   endScreening(date, hall, soldTickets) {
-    const foundIndex = this.screenings.findIndex((el) => el.hall === hall && el.date === date);
+    soldTickets = Number(soldTickets);
+    const foundIndex = this.screenings.findIndex((s) => s.date === date && s.hall === hall);
     if (foundIndex === -1) {
       throw new Error(`Sorry, there is no such screening for ${this.movieName} movie.`);
     }
 
     const currentProfit = soldTickets * this.ticketPrice;
-    this.soldTickets += soldTickets;
     this.totalProfit += currentProfit;
+    this.soldTickets += soldTickets;
     this.screenings.splice(foundIndex, 1);
-
     return `${this.movieName} movie screening on ${date} in ${hall} hall has ended. Screening profit: ${currentProfit}`;
   }
 
   toString() {
-    let output = `${this.movieName} full information:\nTotal profit: ${this.totalProfit.toFixed(0)}$\nSold Tickets: ${
+    return `${this.movieName} full information:\nTotal profit: ${this.totalProfit.toFixed(0)}$\nSold Tickets: ${
       this.soldTickets
-    }\n`;
-
-    if (this.screenings.length) {
-      output += `Remaining film screenings:\n${this.screenings
-        .sort((a, b) => a.hall.localeCompare(b.hall))
-        .map(({ hall, date, description }) => `${hall} - ${date} - ${description}`)
-        .join('\n')}`;
-    } else {
-      output += 'No more screenings!';
-    }
-
-    return output;
+    }${
+      this.screenings.length
+        ? `\nRemaining film screenings:\n${this.screenings
+            .sort((a, b) => a.hall.localeCompare(b.hall))
+            .map(({ date, hall, description }) => `${hall} - ${date} - ${description}`)
+            .join('\n')}`
+        : '\nNo more screenings!'
+    }`;
   }
 }
